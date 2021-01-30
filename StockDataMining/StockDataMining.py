3beccbe5231770b07ecd24a1d8bc9cbba5ea1316
johnMedlockDev <john.medlock.dev@gmail.com>
2021-01-30 17:10:48 -0600

    final has a list of tickers properly formated

--- StockDataMining/StockDataMining.py
+++ StockDataMining/StockDataMining.py
@@ -1,2 +1,11 @@
+from classes.FileIO import FileIO
 
-print('hi there')
+if __name__ == "__main__":
+
+    FILENAME = r'C:\Users\John\source\repos\StockDataMiningSolution\StockDataMining\files\csvs\source\tickers-scrubbed.csv'
+
+    file = FileIO(FILENAME)
+
+    file.CreateInMemoryListFromDataFrame('SYMBOL')
+    result = file.GetInMemoryListFromDataFrame()
+    print(result)
