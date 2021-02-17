615c01cb3f0f82ace8af63e576b33ebc7e9f79a3
johnMedlockDev <john.medlock.dev@gmail.com>
2021-02-16 21:44:29 -0600

    refactored symbolListGen

--- main/classes/SymbolListGenerator.py
+++ main/classes/SymbolListGenerator.py
@@ -17,50 +17,60 @@ class SymbolListGenerator():
 
     def CreateFilteredListOfSymbols(self, directories: list(EJsonFolder)):
 
-        try:
-            self.__parentDirectory__, self.__childDirectory__ = directories
-        except ValueError:
-            self.__parentDirectory__ = directories[0]
-            self.__childDirectory__ = EJsonFolder.NONE
+        self.InitializeDirectories(directories)
 
         listOfPersistedSymbols = self.GetListPersistedOfSymbols()
 
         listOfSymbols = self.__DF__
-        
+
         if self.__childDirectory__ == EJsonFolder.REDO:
             return self.FliterRedoList(listOfPersistedSymbols)
 
         for symbol in listOfPersistedSymbols:
             if symbol in listOfSymbols:
                 listOfSymbols.remove(symbol)
+                
         return listOfSymbols
+
+    def InitializeDirectories(self, directories: list(EJsonFolder)):
+        try:
+            self.__parentDirectory__, self.__childDirectory__ = directories
+        except ValueError:
+            self.__parentDirectory__ = directories[0]
+            self.__childDirectory__ = EJsonFolder.NONE
 
     def GetListPersistedOfSymbols(self):
 
-        jsonPath = f"{Path().absolute()}\\io\\json\\{self.__parentDirectory__.value}"
-
-        symbolsInParent = [f.replace('.json', '') for f in listdir(
-            jsonPath) if isfile(join(jsonPath, f))]
-
-        jsonPath = f"{jsonPath}\\{EJsonFolder.DONE.value}"
-
-        symbolsInDone = [f.replace('.json', '') for f in listdir(
-            jsonPath) if isfile(join(jsonPath, f))]
-
-        listOfPersistedSymbols = list(set(symbolsInParent+symbolsInDone))
+        listOfPersistedSymbols = list(
+            set(self.GetListFromParentDirectory()+self.GetListFromDoneDirectory()))
 
         return listOfPersistedSymbols
 
-    def FliterRedoList(self, listOfPersistedSymbols: list):
-        jsonPath = f"{Path().absolute()}\\io\\json\\{self.__parentDirectory__.value}\\{EJsonFolder.REDO.value}"
+    def GetListFromParentDirectory(self):
+        jsonPath = f"{self.GetParentPath()}"
+        return [f.replace('.json', '') for f in listdir(jsonPath) if isfile(join(jsonPath, f))]
 
-        filesInRedoFolder = [f.replace('.json', '') for f in listdir(
+    def GetListFromDoneDirectory(self):
+        jsonPath = f"{self.GetParentPath()}\{EJsonFolder.DONE.value}"
+        return [f.replace('.json', '') for f in listdir(
             jsonPath) if isfile(join(jsonPath, f))]
+
+    def FliterRedoList(self, listOfPersistedSymbols: list):
+        jsonPath = f"{self.GetParentPath()}\{EJsonFolder.REDO.value}"
+        filesInRedoFolder = self.GetListFromRedoDirectory()
 
         for file in listOfPersistedSymbols:
             if file in filesInRedoFolder:
                 filesInRedoFolder.remove(file)
                 Logger.LogInfo(
-                    f"Removed file {file}.json from {self.__parentDirectory__.value}\{EJsonFolder.REDO.value} because it already exist in {self.__parentDirectory__.value}\done.")
+                    f"Removed file {file}.json from {self.__parentDirectory__.value}\\redo because it already exist in {self.__parentDirectory__.value}\\done.")
                 os.remove(f"{jsonPath}\\{file}.json")
         return filesInRedoFolder
+
+    def GetListFromRedoDirectory(self):
+        jsonPath = f"{self.GetParentPath()}\\{EJsonFolder.REDO.value}"
+        return [f.replace('.json', '') for f in listdir(
+            jsonPath) if isfile(join(jsonPath, f))]
+
+    def GetParentPath(self):
+        return f"{Path().absolute()}\io\json\{self.__parentDirectory__.value}"
